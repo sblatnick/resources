6ed7f651a817c75e264a743727a00687f6fd3ad0
Steven Blatnick <sblatnick@everyone.net>
2015-02-18 16:58:24 -0700

    added chkconfig and runlevel information

--- bash/services.sh
+++ bash/services.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+#::::::::::::::::::::EDITING STARTUP::::::::::::::::::::
+
+#list services and their current status:
+service --status-all
+#add a service (debian):
+update-rc.d apache2 defaults
+#Redhat/CentOS:
+	#list runlevel settings:
+	chkconfig --list
+	#add service:
+	chkconfig --add iptables
+	#delete service:
+	chkconfig --del ip6tables
+	#edit runlevels:
+	chkconfig --level 5 nfsserver off
+	#edit multiple runlevels at once:
+	chkconfig --level 35 nfsserver off
+
+#Run levels:
+Run Level		Mode 															Action
+0 					Halt 															Shuts down system
+1 					Single-User Mode 									Does not configure network interfaces, start daemons, or allow non-root logins
+2 					Multi-User Mode 									Does not configure network interfaces or start daemons.
+3 					Multi-User Mode with Networking 	Starts the system normally.
+4 					Undefined 												Not used/User-definable
+5 					X11 															As runlevel 3 + display manager(X)
+6 					Reboot 														Reboots the system
+
 #::::::::::::::::::::STARTUP::::::::::::::::::::
 
 #startup scripts:
