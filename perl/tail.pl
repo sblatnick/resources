2145e91bd78710cc1a783b7e585e01a46882554a
Steven Blatnick <sblatnick@everyone.net>
2015-02-20 12:08:01 -0700

    tail -f in perl

---
+++ perl/tail.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+use File::Tail;
+
+if(@ARGV == 0) {
+	print "Usage: tailor [file]\n";
+	exit();
+}
+
+my $file = $ARGV[0];
+
+my $ref=tie *FH,"File::Tail",(name=>"$file",interval=>1,maxinterval=>1);
+while (<FH>) {
+  my $line = $_;
+	$line =~ s/.*VerboseDBI.*//g;
+	if($line != "") {
+	  print "$line\n";
+	}
+}
