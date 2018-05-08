30b3b00ba8a24392dc7689e2006e25a9f81b72f5
Steven Blatnick <steve8track@yahoo.com>
2018-05-08 10:26:56 -0600

    LVM help

--- bash/mount_and_devices.sh
+++ bash/mount_and_devices.sh
@@ -2,6 +2,7 @@
 
 #mounted files:
   /etc/fstab
+  #printf "%-23s %-23s %-7s %-15s 0 0" /dev/sda1 "${location}" ext4 "${options}"
 
 #mount ssh:
   sshfs -o cache=no,allow_other,uid=0,gid=0,nonempty steve@vmhost:/home/steve/work /root/work/
@@ -25,3 +26,32 @@
   tc qdisc change dev eth1 root netem delay 100ms 10ms 25%
   #remove:
   tc qdisc del dev eth1 root
+
+
+#LVM - Logical Volume Management
+  fdisk -l #list devices and partitions
+
+  #command prefixes:
+    pv = physical volume
+    vg = volume group
+    lv = logical volume
+
+  #Example: ls volumes:
+    pvs #list physical volumes
+    vgs #list volume groups
+    lvs #list logical volumes
+
+  #Reduce LV size:
+    echo y | lvreduce --resizefs -L -30G /dev/mapper/vgName-lv_name
+
+  #Grow LV size:
+    lvextend --resizefs -L +5G /dev/mapper/vgName-lv_name
+
+  #Create a new LV:
+    lvcreate -L 5G -n lv_name vgName
+
+  #Restore deleted LV:
+    head /etc/lvm/archive/*.vg #look for the one you want to restore
+    vgcfgrestore vgName -f /etc/lvm/archive/vgName_00003-file.vg
+    lvscan
+    lvchange -a y /dev/vgName/lv_name
