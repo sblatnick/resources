8d72aef2d846c19e58d808fd3fda6c58c6be0891
Steven Blatnick <steve8track@yahoo.com>
2019-04-05 11:00:46 -0600

    expect su help, kubernetes sidecar swiss army knife container in busybox

--- bash/expect.sh
+++ bash/expect.sh
@@ -54,3 +54,55 @@ autoexpect commands to run
 
 #see: https://likegeeks.com/expect-command/
 #see also network.sh
+
+#interact after ssh then `su -` with root password within ssh connection:
+  #!/usr/bin/expect -f
+  spawn ssh admin@intra.net
+  expect "Password:"
+  send "password1\r"
+  expect "*$ "
+  send "su -\r"
+  expect "Password:"
+  send "password2\r"
+  expect "*# "
+  interact
+
+#source: https://stackoverflow.com/questions/2823007/ssh-login-with-expect1-how-to-exit-expect-and-remain-in-ssh
+#note: problem with heredoc EOF
+#loop over passwords: https://stackoverflow.com/questions/15526132/expect-ssh-two-possible-passwords-or-how-to-jump-of-expect-block-and-still
+
+#All together:
+
+pw="rootpassword"
+cat << EOF > ${TMP}/to_root
+#!/usr/bin/expect -f
+set passwords { password1 password2 }
+spawn ssh -o StrictHostKeyChecking=no admin\@${node}
+set try 0
+expect {
+  "Password:" {
+    if { \$try >= [llength \$passwords] } {
+      send_error ">>> wrong passwords\n"
+      exit 1
+    }
+
+    send [lindex \$passwords \$try]\r
+    incr try
+    exp_continue
+  }
+  "*\$ " {
+    send "su -\r"
+    expect "Password:"
+    send "${pw}\r"
+    expect "*# "
+    interact
+  }
+  timeout {
+    send_error "time out\n"
+    exit 1
+  }
+}
+EOF
+chmod a+x ${TMP}/to_root
+${TMP}/to_root
+
