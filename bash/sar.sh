56c2297c5a7950be9b485165041c24a6e3693241
Steven Blatnick <steve8track@yahoo.com>
2019-10-31 09:31:19 -0600

    sar grep for reboots

--- bash/sar.sh
+++ bash/sar.sh
@@ -7,3 +7,14 @@
 
   #different days:
   sar -f /var/log/sa/sa09 # 09 = day of month zero padded
+
+  #Show all reboots:
+  for i in $(seq -f "%02g" 1 31)
+  do
+    file=/var/log/sa/sa$i
+    if [ -f $file ];then
+      day=$(stat $file -c %y | cut -d' ' -f1)
+      echo "$day:"
+      sar -f $file | grep 'LINUX RESTART'
+    fi
+  done
