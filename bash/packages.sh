da4a9ff2ab1291d588d2bf500a320ffc93688786
Steven Blatnick <steve8track@yahoo.com>
2020-04-17 23:02:27 -0600

    fixed dockerrc gui a bit, added python pip info

--- bash/packages.sh
+++ bash/packages.sh
@@ -430,3 +430,14 @@ pip install package
 pip install cqlsh #Cassandra Query Language Shell
 pip install 'cqlsh>=5.0.1' --force-reinstall
 
+  #On mac:
+  sudo easy_install pip
+  sudo python -m pip install packages
+  sudo python -m pip install -r requirements.txt
+
+  #requirements file example:
+    requests==2.21.0
+    scp==0.13.2
+    pexpect
+    pyyaml
+
