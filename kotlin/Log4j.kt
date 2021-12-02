45e77287d26e2374263400685baa6fbd71fb7daf
Steven Blatnick <steve8track@yahoo.com>
2021-12-02 09:10:15 -0700

    improved tmpdir util, jira client, added Kotlin Log4j dynamic set level

---
+++ kotlin/Log4j.kt
@@ -0,0 +1,12 @@
+
+
+import org.slf4j.LoggerFactory
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+
+//Programatically set root level:
+val rootLogger: Logger = LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME) as Logger
+rootLogger.setLevel(Level.DEBUG);
+
+//Write to log using DEBUG level:
+log.debug("""${this.javaClass}.createJiraIssue(id: $id) jira: "$jiraIssue"""")
