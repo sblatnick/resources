e7c426c5c402eaf603a6f732327216ffc6a120c9
Steven Blatnick <steve8track@yahoo.com>
2023-05-20 14:27:31 -0600

    filer dup print and better inheritance

--- python/filer/src/files.py
+++ python/filer/src/files.py
@@ -4,13 +4,26 @@ from command import *
 from db import *
 
 class Files(Command):
+  table = 'files'
+
   def __init__(self, option_strings=None, dest=None):
     super().__init__(option_strings, dest)
     action = self.options.action
     db = DB()
     match action:
       case "list":
-        for row in db.query(f"SELECT * FROM files"):
+        for row in db.query(f"SELECT * FROM {self.table}"):
+          print(row)
+      case "dup":
+        for row in db.query(f"""
+          SELECT
+            *,
+            COUNT(md5) AS duplicates
+          FROM {self.table}
+          GROUP BY md5
+          HAVING duplicates > 1
+          ORDER BY duplicates DESC
+        """):
           print(row)
       case _:
         print(f"No such action '{action}'")
--- python/filer/src/images.py
+++ python/filer/src/images.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 import os, argparse, re
-from command import *
+from files import *
 from db import *
 
-class Images(Command):
-  def __init__(self, option_strings=None, dest=None):
+class Images(Files):
+  table = 'images'
+
+  def __init__(self, option_strings=None, dest=None, db=None):
     super().__init__(option_strings, dest)
-    action = self.options.action
-    db = DB()
-    match action:
-      case "list":
-        for row in db.query(f"SELECT * FROM images"):
-          print(row)
-      case _:
-        print(f"No such action '{action}'")
+
