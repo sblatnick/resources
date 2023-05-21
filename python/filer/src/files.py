ac4612fd3727c83bdb06d8157058d89662e2c06e
Steven Blatnick <steve8track@yahoo.com>
2023-05-20 21:15:01 -0600

    filer list duplicate md5sum

--- python/filer/src/files.py
+++ python/filer/src/files.py
@@ -15,15 +15,25 @@ class Files(Command):
         for row in db.query(f"SELECT * FROM {self.table}"):
           print(row)
       case "dup":
+        duplicates = {}
         for row in db.query(f"""
           SELECT
-            *,
-            COUNT(md5) AS duplicates
-          FROM {self.table}
-          GROUP BY md5
-          HAVING duplicates > 1
-          ORDER BY duplicates DESC
+            (
+              SELECT
+                COUNT(md5)
+              FROM {self.table} AS d
+              WHERE d.md5 = t.md5
+            ) AS duplicate,
+            t.md5,
+            t.src
+          FROM {self.table} AS t
+          WHERE duplicate > 1
+          ORDER BY duplicate DESC, t.md5 ASC
         """):
-          print(row)
+          duplicates.setdefault(row["md5"], []).append(row["src"])
+        for md5, sources in duplicates.items():
+          print(md5)
+          for src in sources:
+            print(f"  {src}")
       case _:
         print(f"No such action '{action}'")
