e73ca0396f18cd6910f9908392f7a98fcd714f3c
Steven Blatnick <steve8track@yahoo.com>
2023-05-26 09:38:47 -0600

    filer moved base path into object

--- python/filer/src/scan.py
+++ python/filer/src/scan.py
@@ -11,23 +11,22 @@ class Scan(Command):
     super().__init__(option_strings, dest)
     self.db = DB(recreate=self.recreate)
     signal.signal(signal.SIGINT, self.interrupt_handler)
-    root = os.getcwd()
+    self.root = os.getcwd()
     print("Traversing files")
     for b, dirs, files in os.walk(".", topdown=True):
       base = b[2:]
-      done = self.db.is_done(base)
-      #print(f"'{base}' done: {done}")
       if self.db.is_done(base):
         continue
-      dirs[:] = [d for d in dirs if not self.filter(root, base, d)]
+      dirs[:] = [d for d in dirs if not self.filter(base, d)]
       for filename in files:
-        self.db.add(os.path.join(root,base,filename), self.options.filetype)
+        path = os.path.join(self.root,base,filename)
+        self.db.add(path, self.options.filetype)
         if self.quiting:
           sys.exit(0)
       self.db.add_list('done', base)
 
-  def filter(self, root, base, folder):
-    path = os.path.join(root, base, folder)
+  def filter(self, base, folder):
+    path = os.path.join(self.root, base, folder)
     is_repo = os.path.exists(os.path.join(path, ".git"))
     is_hidden = bool(re.search(r"^\.", folder))
     if is_repo:
