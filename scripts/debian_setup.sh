697ab9bf3fc85a1b36d73ed9585efdc4abfaca23
Steve Blatnick <steve8track@yahoo.com>
2024-11-14 15:08:33 -0700

    setbox configure mate-panel

--- scripts/debian_setup.sh
+++ scripts/debian_setup.sh
@@ -53,6 +53,8 @@ if [ ! -f /usr/bin/ag ];then
     slick-greeter \
     vlc \
     dconf-editor \
+    dconf-cli \
+    colordiff \
     gdebi \
     system-config-printer \
     mozo \
@@ -90,5 +92,109 @@ if grep -q '#autologin-user-timeout' /etc/lightdm/lightdm.conf 2>/dev/null;then
   decrypt | sudo -S sed -i "s/#autologin-user-timeout=0/autologin-user-timeout=0/" /etc/lightdm/lightdm.conf
   decrypt | sudo -S systemctl restart lightdm
 fi
+
+if [ -z "$(which vivaldi)" ];then
+  echo "Installing Vivaldi Browser"
+  curl -o /tmp/dl.html https://vivaldi.com/download/
+  url=$(grep -Po 'https://downloads.vivaldi.com/stable/vivaldi-stable_.*_amd64.deb' /tmp/dl.html)
+  wget ${url}
+  decrypt | sudo -S dpkg -i vivaldi-stable_*.deb
+fi
+
+if ! dconf dump /org/mate/panel/ | grep -q vivaldi;then
+  echo "Adding apps to mate-panel"
+  dconf load /org/mate/panel/ << EOF
+[general]
+object-id-list=['menu-bar', 'notification-area', 'clock', 'show-desktop', 'window-list', 'workspace-switcher', 'object-0', 'object-1', 'object-2']
+toplevel-id-list=['top', 'bottom']
+
+[objects/clock]
+applet-iid='ClockAppletFactory::ClockApplet'
+locked=true
+object-type='applet'
+position=0
+relative-to-edge='end'
+toplevel-id='top'
+
+[objects/clock/prefs]
+custom-format=''
+format='24-hour'
+
+[objects/menu-bar]
+locked=true
+object-type='menu-bar'
+position=0
+toplevel-id='top'
+
+[objects/notification-area]
+applet-iid='NotificationAreaAppletFactory::NotificationArea'
+locked=true
+object-type='applet'
+position=10
+relative-to-edge='end'
+toplevel-id='top'
+
+[objects/object-0]
+launcher-location='/usr/share/applications/vivaldi-stable.desktop'
+object-type='launcher'
+position=-1
+toplevel-id='top'
+
+[objects/object-1]
+action-type='force-quit'
+object-type='action'
+position=229
+relative-to-edge='end'
+toplevel-id='top'
+
+[objects/object-2]
+applet-iid='MultiLoadAppletFactory::MultiLoadApplet'
+object-type='applet'
+position=199
+relative-to-edge='end'
+toplevel-id='top'
+
+[objects/object-2/prefs]
+view-memload=true
+
+[objects/show-desktop]
+applet-iid='WnckletFactory::ShowDesktopApplet'
+locked=true
+object-type='applet'
+position=0
+toplevel-id='bottom'
+
+[objects/window-list]
+applet-iid='WnckletFactory::WindowListApplet'
+locked=true
+object-type='applet'
+position=20
+toplevel-id='bottom'
+
+[objects/workspace-switcher]
+applet-iid='WnckletFactory::WorkspaceSwitcherApplet'
+locked=true
+object-type='applet'
+position=0
+relative-to-edge='end'
+toplevel-id='bottom'
+
+[toplevels/bottom]
+expand=true
+orientation='bottom'
+screen=0
+size=24
+y=999
+y-bottom=0
+
+[toplevels/top]
+expand=true
+orientation='top'
+screen=0
+size=24
+EOF
+  killall mate-panel
+fi
+
 
 echo "DONE"
